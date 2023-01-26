b525829ebda1219b2ead031ccc6cbae9185817ec
AiriNarita <airiswim.kitty@gmail.com>
2023-01-26 18:44:15 +0900

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
 
+  resources :books do
+    resources :favorites , only: [:create , :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
