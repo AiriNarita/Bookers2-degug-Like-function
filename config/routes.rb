bb8d72e01596f5c83c06e546371daaaeeca1dfd1
AiriNarita <airiswim.kitty@gmail.com>
2023-01-26 19:25:44 +0900

    [add]like

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'favolites/create'
+  get 'favolites/new'
+  get 'favolites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
---
+++ test/controllers/favolites_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class FavolitesControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get favolites_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get favolites_new_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get favolites_destroy_url
+    assert_response :success
+  end
+end
