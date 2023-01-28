d703295d20d7581747ebc394f3d904eeaa792f3c
AiriNarita <airiswim.kitty@gmail.com>
2023-01-28 17:58:12 +0900

    [fix]Relashonship Controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,16 @@
 class RelationshipsController < ApplicationController
 
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+  
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+
+
   def follows
     user = User.find(params[:id])
     @users = user.following_user.page(params[:page]).per(3).reverse_order
