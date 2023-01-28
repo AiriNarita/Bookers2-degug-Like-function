2daf962a0bd104957c202e3fda2778867da07178
AiriNarita <airiswim.kitty@gmail.com>
2023-01-28 17:30:59 +0900

    [add]assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,30 @@ class User < ApplicationRecord
    has_many :favorites, dependent: :destroy
    has_many :book_comments, dependent: :destroy
 
+  #relationshipModel assosiation
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # ① フォローしている人取得(Userのfollowerから見た関係)
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # ② フォローされている人取得(Userのfolowedから見た関係)
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction,length: { maximum: 50 }
 
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
 
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
 
-  
-  
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+ 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
