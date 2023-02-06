76424bef0fb24de554381cef3f6c9f9f2cd46cf6
AiriNarita <airiswim.kitty@gmail.com>
2023-02-06 14:57:05 +0900

    feat: routing

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   get 'searches/search', to: 'searches#search'
 
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
