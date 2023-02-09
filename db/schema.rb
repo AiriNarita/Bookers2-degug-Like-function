8b212855f0e279101b3dfb1faaf10dedf7672122
AiriNarita <airiswim.kitty@gmail.com>
2023-02-09 21:20:06 +0900

    feat: star-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_07_005538) do
+ActiveRecord::Schema.define(version: 2023_02_09_090127) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_02_07_005538) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "star"
   end
 
   create_table "entries", force: :cascade do |t|
