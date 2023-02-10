3564838aa533a9c2c51ba9c12294fdaca1c97385
AiriNarita <airiswim.kitty@gmail.com>
2023-02-10 17:07:42 +0900

    feat: categorycolumn/validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -96,7 +96,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :image, :star)
+    params.require(:book).permit(:title, :body, :image, :star,:category)
   end
 end
 
